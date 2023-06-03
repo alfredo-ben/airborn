a8b356c7b2a3fbb820373300527ab81ab4d2d5c5
alfredo-ben <alfredo@thanksben.com>
2023-06-03 16:11:07 +0200

    adding latest status of django rust replacement

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(decl_macro)]
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-use rocket_contrib::databases::diesel;
-use rocket::Request;
-use rocket::response::content::Json;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 use rocket::request::Form;
+use rocket::response::content::Json;
+use rocket::Request;
+use rocket_contrib::databases::diesel;
 use rocket_contrib::templates::Template;
 use serde::Serialize;
 
@@ -13,33 +15,34 @@ struct LogsDbConn(diesel::PgConnection);
 
 #[derive(FromForm, Debug)]
 struct Book {
-  title: String,
-  author: String,
-  isbn: String
+    title: String,
+    author: String,
+    isbn: String,
 }
 
 #[derive(Serialize)]
-  struct Context {
+struct Context {
     first_name: String,
-    last_name: String
-  }
+    last_name: String,
+}
 
 #[get("/")]
 fn index() -> Template {
-
-  let context = Context {
-    first_name: String::from("Alfredo"),
-    last_name: String::from("Suarez")
-  };
-  Template::render("home", context)
+    let context = Context {
+        first_name: String::from("Alfredo"),
+        last_name: String::from("Suarez"),
+    };
+    Template::render("home", context)
 }
 
 #[get("/hello")]
 fn hello() -> Json<&'static str> {
-  Json("{
+    Json(
+        "{
     'status': 'success',
     'message': 'Hello API!'
-  }")
+  }",
+    )
 }
 
 #[catch(404)]
@@ -50,17 +53,17 @@ fn not_found(req: &Request) -> String {
 
 #[post("/book", data = "<book_form>")]
 fn new_book(book_form: Form<Book>) -> String {
-  let book: Book = book_form.into_inner();
-  let mut dummy_db: Vec<Book> = Vec::new();
-  dummy_db.push(book);
-  format!("Book added successfully: {:?}", dummy_db)
+    let book: Book = book_form.into_inner();
+    let mut dummy_db: Vec<Book> = Vec::new();
+    dummy_db.push(book);
+    format!("Book added successfully: {:?}", dummy_db)
 }
 
 fn main() {
-  rocket::ignite()
-    .register(catchers![not_found])
-    .mount("/", routes![index])
-    .mount("/api", routes![hello, new_book])
-    .attach(Template::fairing())
-    .launch();
+    rocket::ignite()
+        .register(catchers![not_found])
+        .mount("/", routes![index])
+        .mount("/api", routes![hello, new_book])
+        .attach(Template::fairing())
+        .launch();
 }
